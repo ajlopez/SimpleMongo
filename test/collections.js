06029aa88888d5df5f944363cdaa918be8dac049
ajlopez <webmaster@ajlopez.com>
2013-08-09 10:54:15 -0300

    Insert first test

--- test/collections.js
+++ test/collections.js
@@ -7,3 +7,10 @@ var collections = require('../lib/collections'),
 var collection = collections.createCollection('people');
 assert.ok(collection);
 assert.equal(collection.name, 'people');
+
+// insert data
+
+var adam = { name: 'Adam' };
+collection.insert(adam);
+assert.ok(adam._id);
+
