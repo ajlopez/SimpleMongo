e29e84ee8a6cd7f0e4236ce39b06d1fbefa34255
ajlopez <webmaster@ajlopez.com>
2013-08-10 18:58:04 -0300

    clone document at add

--- test/collections.js
+++ test/collections.js
@@ -14,3 +14,39 @@ var adam = { name: 'Adam' };
 collection.insert(adam);
 assert.ok(adam._id);
 
+// retrieve data
+
+var people = collection.find();
+assert.ok(people);
+assert.ok(Array.isArray(people));
+assert.equal(people.length, 1);
+assert.equal(people[0]._id, adam._id);
+assert.equal(people[0].name, adam.name);
+
+// insert second document
+
+var eve = { name: 'Eve' };
+collection.insert(eve);
+assert.ok(eve._id);
+
+// then retrieve data
+
+var people = collection.find();
+assert.ok(people);
+assert.ok(Array.isArray(people));
+assert.equal(people.length, 2);
+assert.equal(people[0]._id, adam._id);
+assert.equal(people[0].name, adam.name);
+assert.equal(people[1]._id, eve._id);
+assert.equal(people[1].name, eve.name);
+
+// change original document and retrieve it
+
+adam.name = "New Adam";
+
+var people = collection.find();
+assert.ok(people);
+assert.ok(Array.isArray(people));
+assert.equal(people.length, 2);
+assert.equal(people[0]._id, adam._id);
+assert.equal(people[0].name, "Adam");
