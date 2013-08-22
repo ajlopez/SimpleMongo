2e40409848d48dbab25e70f24d7d2c4cca9e1747
ajlopez <webmaster@ajlopez.com>
2013-08-22 10:21:46 -0300

    Refactor tests to use simpleunit

--- test/collections.js
+++ test/collections.js
@@ -1,64 +1,90 @@
 
-var collections = require('../lib/collections'),
-    assert = require('assert');
+var collections = require('../lib/collections');
 
-// creates collection
+exports['creates collection'] = function (test) {
+    var collection = collections.createCollection('people');
+    test.ok(collection);
+    test.equal(collection.name, 'people');
+}
+
+exports['insert data'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    test.ok(adam._id);
+}
+
+exports['retrieve data'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var people = collection.find();
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 1);
+    test.equal(people[0]._id, adam._id);
+    test.equal(people[0].name, adam.name);
+}
+
+exports['insert second document'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
+    test.ok(eve._id);
+}
+
+exports['then retrieve data'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
     
-var collection = collections.createCollection('people');
-assert.ok(collection);
-assert.equal(collection.name, 'people');
+    var people = collection.find();
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 2);
+    test.equal(people[0]._id, adam._id);
+    test.equal(people[0].name, adam.name);
+    test.equal(people[1]._id, eve._id);
+    test.equal(people[1].name, eve.name);
+}
 
-// insert data
+exports['change original document and retrieve it'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
 
-var adam = { name: 'Adam' };
-collection.insert(adam);
-assert.ok(adam._id);
+    adam.name = "New Adam";
 
-// retrieve data
+    var people = collection.find();
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 2);
+    test.equal(people[0]._id, adam._id);
+    test.equal(people[0].name, "Adam");
+}
 
-var people = collection.find();
-assert.ok(people);
-assert.ok(Array.isArray(people));
-assert.equal(people.length, 1);
-assert.equal(people[0]._id, adam._id);
-assert.equal(people[0].name, adam.name);
+exports['change retrieved document and retrieve it again'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
+    
+    var people = collection.find();
+    adam = people[0];
+    adam.name = "New Adam";
 
-// insert second document
+    var people = collection.find();
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 2);
+    test.equal(people[0]._id, adam._id);
+    test.equal(people[0].name, "Adam");
+}
 
-var eve = { name: 'Eve' };
-collection.insert(eve);
-assert.ok(eve._id);
-
-// then retrieve data
-
-var people = collection.find();
-assert.ok(people);
-assert.ok(Array.isArray(people));
-assert.equal(people.length, 2);
-assert.equal(people[0]._id, adam._id);
-assert.equal(people[0].name, adam.name);
-assert.equal(people[1]._id, eve._id);
-assert.equal(people[1].name, eve.name);
-
-// change original document and retrieve it
-
-adam.name = "New Adam";
-
-var people = collection.find();
-assert.ok(people);
-assert.ok(Array.isArray(people));
-assert.equal(people.length, 2);
-assert.equal(people[0]._id, adam._id);
-assert.equal(people[0].name, "Adam");
-
-// change retrieved document and retrieve it again
-
-adam = people[0];
-adam.name = "New Adam";
-
-var people = collection.find();
-assert.ok(people);
-assert.ok(Array.isArray(people));
-assert.equal(people.length, 2);
-assert.equal(people[0]._id, adam._id);
-assert.equal(people[0].name, "Adam");
