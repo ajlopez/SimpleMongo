15074eddcbd5c10170e015cc93f59c816da76bcf
ajlopez <webmaster@ajlopez.com>
2013-08-29 17:51:16 -0300

    find with query, first test

--- test/find.js
+++ test/find.js
@@ -29,6 +29,7 @@ exports['find two documents'] = function (test) {
     test.equal(people[1]._id, eve._id);
     test.equal(people[1].name, eve.name);
 }
+
 exports['change original document and find it'] = function (test) {
     var collection = collections.createCollection('people');
     var adam = { name: 'Adam' };
@@ -63,5 +64,21 @@ exports['change retrieved document and find it again'] = function (test) {
     test.equal(people.length, 2);
     test.equal(people[0]._id, adam._id);
     test.equal(people[0].name, "Adam");
+}
+
+exports['find with query by example'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
+
+    var people = collection.find({ name: 'Eve' });
+    
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 1);
+    test.equal(people[0]._id, eve._id);
+    test.equal(people[0].name, "Eve");
 }
 
