93289edacac6b3463e0f94d6d0b034f647104e6c
ajlopez <webmaster@ajlopez.com>
2013-09-12 19:14:16 -0300

    Remove with justone

--- test/remove.js
+++ test/remove.js
@@ -14,6 +14,30 @@ exports['remove one using query'] = function (test) {
     test.equal(people[0].name, "Eve");
 }
 
+exports['remove all'] = function (test) {
+    var collection = createCollection();
+    
+    collection.remove();
+
+    var people = collection.find();
+    
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 0);
+}
+
+exports['remove first'] = function (test) {
+    var collection = createCollection();
+    
+    collection.remove(null, true);
+
+    var people = collection.find();
+    
+    test.ok(people);
+    test.ok(Array.isArray(people));
+    test.equal(people.length, 1);
+}
+
 function createCollection() {
     var collection = collections.createCollection('people');
     var adam = { name: 'Adam' };
