d8432445157a9bf08f69cc8ae2f1d42819e34542
ajlopez <webmaster@ajlopez.com>
2013-09-19 17:03:52 -0300

    Collection count first tests

--- test/collections.js
+++ test/collections.js
@@ -23,3 +23,28 @@ exports['insert second document'] = function (test) {
     test.ok(eve._id);
 }
 
+exports['count after two inserts'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
+    test.equal(2, collection.count());
+}
+
+exports['count after two inserts and one remove'] = function (test) {
+    var collection = collections.createCollection('people');
+    var adam = { name: 'Adam' };
+    collection.insert(adam);
+    var eve = { name: 'Eve' };
+    collection.insert(eve);
+    collection.remove({ name: 'Adam' });
+    test.equal(1, collection.count());
+}
+
+exports['count on empty collection'] = function (test) {
+    var collection = collections.createCollection('people');
+    test.equal(0, collection.count());
+}
+
+
